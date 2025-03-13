31466b7985c0cd71299e506df30dddae974f222c
Marcus Linhares <marcusviniciuslinhares543@gmail.com>
2025-03-13 07:36:52 -0300

    add webview

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,35 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'viewmodels/product_viewmodel.dart';
-import 'views/cadastro_produto_page.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  late final WebViewController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..loadRequest(
+          Uri.parse('https://flyer-admin-ui-cjwsfe.flutterflow.app/'));
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (_) => ProductViewModel()),
-      ],
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Flyer App',
-        home: const CadastroProdutoPage(),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: WebViewWidget(controller: _controller),
       ),
     );
   }
